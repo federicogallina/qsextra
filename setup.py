1fee855e409313b61672936194bd670edf7dcd68
Federico Gallina <federicogallina.fg@gmail.com>
2023-02-13 12:28:17 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qsextra',
+    version=0.1,
+    author='Federico Gallina',
+    author_email='federico.gallina@unipd.it',
+    description='Quantum Simulation of Exciton Transport',
+    long_description='Simulate exciton transport in molecular network in  presence of a dephasing environment. It is based on circuit quantum computing using the IBM Qiskit suite.',
+    packages=find_packages(),
+    url='https://github.com/federicogallina/qsextra',
+    install_requires=['numpy','qiskit']
+)
