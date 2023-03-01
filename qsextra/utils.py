b7165a8ec2378b02c41e6a5970de3888aa98d32c
Federico Gallina <federicogallina.fg@gmail.com>
2023-03-01 13:52:03 +0100

    Creation of utils

---
+++ qsextra/utils.py
@@ -0,0 +1,39 @@
+from qiskit import QuantumCircuit, QuantumRegister
+import numpy as np
+
+def CNOT_staircase_circuit(qubits, dt, Pauli_dict):
+    '''A simple routine to generate a CNOT staircase circuit implementing some Pauli rotations.
+    Input:
+    - qubits [int]: number of qubits of the system
+    - dt [float, double]: time step of evolution
+    - Pauli_dict [dictionary]: dictionary of Pauli strings and relative coefficients'''
+    
+    q_reg = QuantumRegister(qubits)
+    qc = QuantumCircuit(q_reg)
+    for Pauli_string, Pauli_coef in Pauli_dict.items():
+        last_qubit = None
+        qc_rotations = QuantumCircuit(q_reg)
+        for npauli, pauli in enumerate(Pauli_string):
+            if pauli == 'X':
+                qc_rotations.h(npauli)
+            elif pauli == 'Y':
+                qc_rotations.rx(np.pi/2, npauli)
+        qc_stairs = QuantumCircuit(q_reg)
+        for npauli, pauli in enumerate(Pauli_string):
+            if pauli != 'I':
+                last_qubit = npauli
+                finder = False
+                index = npauli + 1
+                while finder == False and index < qubits:
+                    if Pauli_string[index] == 'I':
+                        index = index + 1
+                    else:
+                        finder = True
+                        qc_stairs.cx(npauli, index)
+        if last_qubit != None:
+            qc.compose(qc_rotations,inplace=True)
+            qc.compose(qc_stairs,inplace=True)
+            qc.rz(Pauli_coef * dt, last_qubit)
+            qc.compose(qc_stairs.inverse(),inplace=True)
+            qc.compose(qc_rotations.inverse(),inplace=True)
+    return qc
