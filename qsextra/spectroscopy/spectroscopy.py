f1a90c74b7918fe8dc68f080ef5e7779e54c292b
Federico Gallina <federicogallina.fg@gmail.com>
2024-03-07 16:52:55 +0100

    spectroscopy

--- qsextra/spectroscopy/spectroscopy.py
+++ qsextra/spectroscopy/spectroscopy.py
@@ -18,14 +18,44 @@ class Spectroscopy():
         self._label = label
 
     @property
-    def delay_time(self)->np.ndarray:
+    def delay_time(self)->list[list]:
         return self._delay_time
 
     @delay_time.setter
     def delay_time(self,
-                   delay,
+                   delays,
                    ):
-        self._delay_time = np.array(delay)
+        ''' Convert the input to a list of lists:
+
+        Parameters
+        ----------
+        delays: float | list[float] | numpy.ndarray | list[numpy.ndarray] | list[list[float]]
+            - `float`: `T` is converted to `[[T]]`.
+            - `list[float]`: `[Ta, Tb, Tc]` is converted to `[[Ta, Tb, Tc]]`.
+            - `numpy.ndarray`: `numpy.array([Ta, Tb, Tc])` is converted to `[[Ta, Tb, Tc]]`.
+            - `list[numpy.ndarray]`: `[numpy.array([T1a, T1b, T1c]), numpy.array([T2a, T2b, T2c]), numpy.array([T3a, T3b, T3c])]` is converted to `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]`.
+            - `list[list[float]]`: `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]` is preserved.
+        '''
+        if np.isscalar(delays):
+            dys = [[delays]]
+        elif isinstance(delays, np.ndarray):
+            dys = [delays.tolist()]
+        elif isinstance(delays, list):
+            if all([np.isscalar(delay) for delay in delays]):
+                dys = [delays]
+            else:
+                dys = []
+                for delay in delays:
+                    if np.isscalar(delay):
+                        dy = [delay]
+                    elif isinstance(delay, np.ndarray):
+                        dy = delay.tolist()
+                    elif isinstance(delay, list):
+                        dy = delay
+                    dys.append(dy)
+        else:
+            raise TypeError('delay_time is a {}. However, a list, numpy.ndarray or a float are expected.'.format(type(delays)))
+        self._delay_time = dys
 
     @property
     def side_seq(self)->str:
@@ -51,10 +81,10 @@ class Spectroscopy():
                  delay_time: np.ndarray | list[list[float]] | list[float] | float,
                  label: str = "",
                  ):
-        self._label = label
-        self._delay_time = delay_time
-        self._side_seq = None
-        self._direction_seq = None
+        self.label = label
+        self.delay_time = delay_time
+        self.side_seq = ''
+        self.direction_seq = ''
 
     def todict(self) -> dict:
         '''
@@ -74,12 +104,12 @@ class Spectroscopy():
         return sp_dict
 
 class FeynmanDiagram(Spectroscopy):
-    _linear_name_list = ["a", "abs"]
-    _thirdorder_name_list = ["gsb", "se", "esa"]
+    _linear_list = ["a", "abs"]
+    _thirdorder_list = ["gsb", "se", "esa"]
 
     def __init__(self,
                  FD_label: str,
-                 delay_time: np.ndarray | list[list[float]] | list[float] | float,
+                 delay_time: float | list[float] | np.ndarray | list[np.ndarray] | list[list[float]],
                  ):
         '''
         Create an object that contains the information about the contribution of the response function to be simulated.
@@ -94,8 +124,15 @@ class FeynmanDiagram(Spectroscopy):
             - `"se"`:  Third-order rephasing Stimulated Emission
             - `"esa"`: Third-order rephasing Excited State Absorption
 
-        delay_time: numpy.ndarray | list[list[float]] | list[float] | float
-            A list with the delay times. For the linear absorption the input is: `list[float]` or `float`. For third-order responses the input is a list with 3 entres (i.e.: `[T1_list, T2_list, T3_list]`), therefore the accepted types are: `list[list[float]]` or `list[float]`.
+        delay_time: float | list[float] | numpy.ndarray | list[numpy.ndarray] | list[list[float]]
+            A list with the delay times.
+            For the linear absorption the input is: `float`, `list[float]` or `numpy.ndarray`.
+            For third-order responses the input is a list with 3 lists (i.e.: `[[T1], [T2], [T3]]`), therefore the accepted types are: `list[list[float]]` or `list[numpy.ndarray]`.
+            - `float`: `T` is converted to `[[T]]`.
+            - `list[float]`: `[Ta, Tb, Tc]` is converted to `[[Ta, Tb, Tc]]`.
+            - `numpy.ndarray`: `numpy.array([Ta, Tb, Tc])` is converted to `[[Ta, Tb, Tc]]`.
+            - `list[numpy.ndarray]`: `[numpy.array([T1a, T1b, T1c]), numpy.array([T2a, T2b, T2c]), numpy.array([T3a, T3b, T3c])]` is converted to `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]`.
+            - `list[list[float]]`: `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]` is preserved.
         '''
         super.__init__()
         self.set(FD_label, delay_time)
@@ -116,7 +153,7 @@ class FeynmanDiagram(Spectroscopy):
         
     def set(self,
             FD_label: str,
-            delay_time: np.ndarray | list[list[float]] | list[float] | float,
+            delay_time: float | list[float] | np.ndarray | list[np.ndarray] | list[list[float]],
             ):
         '''
         Create an object that contains the information about the contribution of the response function to be simulated.
@@ -131,39 +168,32 @@ class FeynmanDiagram(Spectroscopy):
             - `"se"`:  Third-order rephasing Stimulated Emission
             - `"esa"`: Third-order rephasing Excited State Absorption
 
-        delay_time: numpy.ndarray | list[list[float]] | list[float] | float
-            A list with the delay times. For the linear absorption the input is: `list[float]` or `float`. For third-order responses the input is a list with 3 entres (i.e.: `[T1_list, T2_list, T3_list]`), therefore the accepted types are: `list[list[float]]` or `list[float]`.
+        delay_time: float | list[float] | numpy.ndarray | list[numpy.ndarray] | list[list[float]]
+            A list with the delay times.
+            For the linear absorption the input is: `float`, `list[float]` or `numpy.ndarray`.
+            For third-order responses the input is a list with 3 lists (i.e.: `[[T1], [T2], [T3]]`), therefore the accepted types are: `list[list[float]]` or `list[numpy.ndarray]`.
+            - `float`: `T` is converted to `[[T]]`.
+            - `list[float]`: `[Ta, Tb, Tc]` is converted to `[[Ta, Tb, Tc]]`.
+            - `numpy.ndarray`: `numpy.array([Ta, Tb, Tc])` is converted to `[[Ta, Tb, Tc]]`.
+            - `list[numpy.ndarray]`: `[numpy.array([T1a, T1b, T1c]), numpy.array([T2a, T2b, T2c]), numpy.array([T3a, T3b, T3c])]` is converted to `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]`.
+            - `list[list[float]]`: `[[T1a, T1b, T1c], [T2a, T2b, T2c], [T3a, T3b, T3c]]` is preserved.
         '''
         # Checking the Feynman diagram.
         FD_label = FD_label.casefold()
         if not isinstance(FD_label, str):
             raise TypeError()
-        if FD_label not in self._linear_name_list and FD_label not in self._thirdorder_name_list:
+        if FD_label not in self._linear_list and FD_label not in self._thirdorder_list:
             raise ValueError('Unsupported Feynman diagram.')
-        # Checking the correct input format for delay_time. Converting float to list if necessary.
-        if FD_label in self._linear_name_list:
-            delay_time = if_scalar_to_list(delay_time)
-            if isinstance(delay_time, np.ndarray):
-                delay_time = delay_time.tolist()
-            elif not isinstance(delay_time, list):
-                raise TypeError('delay_time is a {}. However, a list, numpy.ndarray or a float are expected.'.format(type(delay_time)))
-        elif FD_label in self._thirdorder_name_list:
-            if isinstance(delay_time, np.ndarray):
-                delay_time = delay_time.tolist()
-            if not isinstance(delay_time, list):
-                raise TypeError('delay_time is a {}. However, a list is expected.'.format(type(delay_time)))
-            if len(delay_time) != 3:
-                raise ValueError('delay_time contains {} items. However, 3 are expected.'.format(len(delay_time)))
-            for i, T_i in enumerate(delay_time):
-                delay_time[i] = if_scalar_to_list(T_i)
-                if isinstance(T_i, np.ndarray):
-                    delay_time[i] = T_i.tolist()
-                elif not isinstance(T_i, list):
-                    raise TypeError('delay_time[{}] is a {}. However, a list, numpy.ndarray or a float are expected.'.format(i, type(T_i)))
-        
-        # Saving type of Feynamn diagram and the list of delay times.
         self.label = FD_label
+        
+        # Checking the correct input format for delay_time. Converting float to list if necessary.
         self.delay_time = delay_time
-
+        if self.label in self._linear_list:
+            if len(self.delay_time) != 1:
+                raise ValueError('delay_time contains {} items. However, 1 is expected.'.format(len(self.delay_time)))
+        if self.label in self._thirdorder_list:
+            if len(self.delay_time) != 3:
+                raise ValueError('delay_time contains {} items. However, 3 are expected.'.format(len(self.delay_time)))
+            
         # Generate the associated sequences
         self.__FD_to_sequence()
