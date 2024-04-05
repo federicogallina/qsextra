7a2541514a7d2900486c76e40985c13f5bdcb1d6
Federico Gallina <52489758+federicogallina@users.noreply.github.com>
2024-04-05 09:38:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ setup(
     author='Federico Gallina',
     author_email='federico.gallina@unipd.it',
     description='Quantum Simulation of Exciton Transport',
-    long_description='Simulate exciton transport in molecular network in  presence of an environment.',
+    long_description='Simulate exciton transport in molecular networks and their time-resolved electronic spectra.',
     packages=find_packages(),
     url='https://github.com/federicogallina/qsextra',
-    install_requires=['numpy==1.26.4','scipy==1.12','matplotlib','qiskit','qiskit-aer','qutip==4.7.5']
+    install_requires=['numpy==1.26.4',
+                      'scipy==1.12',
+                      'matplotlib',
+                      'qiskit==1.0.0',
+                      'qiskit-aer==0.13.3',
+                      'qutip==4.7.5',
+                     ]
 )
